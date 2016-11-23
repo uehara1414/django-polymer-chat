824761ab49db36f286b21d36fc7f22ad0bd68dbb
uehara1414 <b1015062@fun.ac.jp>
2016-11-24 01:46:51 +0900

    Front-End's Hello World

--- app/cardchat/views.py
+++ app/cardchat/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import JsonResponse
 
 def hello_world(request):
-    return JsonResponse({"status": "hello world"})
+    return JsonResponse({"status": "ok."})
